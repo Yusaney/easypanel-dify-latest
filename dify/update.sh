587def33c370fdaf5189c967c553de4fc2c1602b
Redu <83190322+Yusaney@users.noreply.github.com>
2024-11-05 18:12:10 +0100

    Update update.sh

--- dify/update.sh
+++ dify/update.sh
@@ -45,4 +45,51 @@ echo "Copying new files from docker directory..."
 rm -rf "$TARGET_DIR"/*
 cp -r "$REPO_DIR/docker/." "$TARGET_DIR"
 
+# Function to modify docker-compose files for Easypanel compatibility
+modify_for_easypanel() {
+    local file="$1"
+    echo "Modifying $file for Easypanel compatibility..."
+    
+    # Create a temporary file
+    temp_file=$(mktemp)
+    
+    # Process the file line by line
+    awk '
+    BEGIN { in_ports = 0; skip_line = 0; }
+    {
+        # Skip container_name lines
+        if ($1 == "container_name:") {
+            next
+        }
+        
+        # Handle ports sections
+        if ($1 == "ports:") {
+            print "    # ports:"
+            in_ports = 1
+            next
+        }
+        
+        # If we are in a ports section, comment out the lines
+        if (in_ports && $1 ~ /^[[:space:]]*-/) {
+            print "    #" $0
+            next
+        }
+        
+        # If we hit a line that is not indented more than ports, we are out of the ports section
+        if (in_ports && $1 !~ /^[[:space:]]*-/) {
+            in_ports = 0
+        }
+        
+        # Print all other lines normally
+        print $0
+    }' "$file" > "$temp_file"
+    
+    # Replace original file with modified version
+    mv "$temp_file" "$file"
+}
+
+# Find and modify all docker-compose files
+echo "Modifying docker-compose files for Easypanel..."
+find "$TARGET_DIR" -type f -name "docker-compose*.yml" -exec bash -c 'modify_for_easypanel "$0"' {} \;
+
 echo "Update completed successfully!"
